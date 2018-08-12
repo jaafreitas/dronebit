dfc8925bac68665037ea27626c6dca41bf8a2e2d
João Adriano Freitas <jaafreitas@gmail.com>
2018-08-12 13:53:31 -0300

    Versão inicial do protótipo.

---
+++ microbitDroneProtótipo.js
@@ -0,0 +1,43 @@
+let velocidade_max = 0
+let velocidade_min = 0
+let tras_frente = 0
+let horario_anti_horario = 0
+let esquerda_direita = 0
+let velocidade = 0
+let ligado = false
+input.onButtonPressed(Button.AB, () => {
+    if (ligado) {
+        ligado = false
+        velocidade = 0
+    } else {
+        ligado = true
+        velocidade = 100
+    }
+    esquerda_direita = 0
+    horario_anti_horario = 0
+    tras_frente = 0
+})
+ligado = false
+velocidade = 0
+velocidade_min = 50
+velocidade_max = 200
+esquerda_direita = 0
+horario_anti_horario = 0
+tras_frente = 0
+basic.forever(() => {
+    if (ligado) {
+        // viro horário e anti-horário
+        if (input.buttonIsPressed(Button.A)) {
+            horario_anti_horario = input.acceleration(Dimension.X) / 10
+        } else if (input.buttonIsPressed(Button.B)) {
+            velocidade = 100 - input.acceleration(Dimension.Y) / 10
+        } else {
+            tras_frente = input.acceleration(Dimension.Y) / 10
+            esquerda_direita = input.acceleration(Dimension.X) / 10
+        }
+    }
+    led.plotBrightness(0, 0, velocidade + esquerda_direita + tras_frente + horario_anti_horario)
+    led.plotBrightness(4, 0, velocidade - esquerda_direita + tras_frente - horario_anti_horario)
+    led.plotBrightness(0, 4, velocidade + esquerda_direita - tras_frente - horario_anti_horario)
+    led.plotBrightness(4, 4, velocidade - esquerda_direita - tras_frente + horario_anti_horario)
+})
